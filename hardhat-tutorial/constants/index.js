584424ab21ac87c668d9d71fe3c97eb7a408fe62
Phan <pha101@csiro.au>
2022-03-03 22:39:16 +1000

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x985C524d4F34703634750D48F63136565CD9713f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
